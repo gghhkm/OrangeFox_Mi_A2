c06939b207e633be14fe3af4f468552fdb2ed46b
test <test@test.com>
2021-01-07 00:44:55 -0500

    clean up, configs still need work.

--- compile.sh
+++ compile.sh
@@ -3,22 +3,22 @@ echo " Hello, Thank you for using this script "
 echo " You can easily build orange fox with this script for your Xiaomi Device "
 echo " First lets setup the environment "
 echo " Press Enter to Start "
-read Ans
+read -r Ans
 
 echo "_________________________________________________________________________________________"
 
-cd
+cd || return
 git clone https://github.com/akhilnarang/scripts
 bash scripts/setup/android_build_env.sh
 
 echo "_________________________________________________________________________________________"
 
 echo " Now lets sync the Latest Orange Fox Sources [ Latest 9.0 ] "
-read ans 
-cd
-cd scripts
+read -r ans
+cd || return
+cd scripts || return
 mkdir Orangefox
-cd Orangefox
+cd Orangefox || return
 repo init --depth=1 -q -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
 repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
@@ -29,29 +29,29 @@ clear
 echo "_________________________________________________________________________________________"
 
 echo " Now tell me your device codename "
-read code 
+read -r code
 
 echo " Now tell me your device vendor "
-read vendor
+read -r vendor
 echo " Now Give me your trees github link "
-read trees
+read -r trees
 
 
-cd
-cd scripts/Orangefox
-git clone $trees device/$vendor/$code
+cd || return
+cd scripts/Orangefox || return
+git clone "$trees" device/"$vendor"/"$code"
 
 echo "_________________________________________________________________________________________"
 
 echo " Now lets start building the environment "
-cd
-cd scripts/Orangefox/
+cd || return
+cd scripts/Orangefox/ || return
 source build/envsetup.sh
 
 echo "_________________________________________________________________________________________"
 
 echo " Now tell me the name of the maintainer "
-read main
+read -r main
 
 echo "_________________________________________________________________________________________"
 
@@ -73,12 +73,13 @@ clear
 echo "_________________________________________________________________________________________"
 
 echo " Lets Lunch it all together 😉😋 "
-lunch omni_$"{code}"
+lunch omni_"${code}"
 
 # If lunch command fail, there is no need to continue building
 if [ "$?" != "0" ]; then
-	exit
+  exit
 fi
+
 
 
 echo "_________________________________________________________________________________________"
