edfe31496b1445d688cff32cc987a8053a2f0446
gghhkm <killk4809@protonmail.com>
2021-01-09 13:58:49 -0500

    Much Clean. Much Good. Much YaY!

--- compile.sh
+++ compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 echo "
+.█████╗.███████╗.█████╗.██╗..██╗.
+██╔══██╗██╔════╝██╔══██╗╚██╗██╔╝....
+██║..██║█████╗..██║..██║.╚███╔╝...
+██║..██║██╔══╝..██║..██║.██╔██╗....
+╚█████╔╝██║.....╚█████╔╝██╔╝╚██╗..
+.╚════╝.╚═╝......╚════╝.╚═╝..╚═╝....
 .█████╗..█████╗.███╗...███╗██████╗.██╗██╗.....███████╗....██╗.......██╗██╗████████╗██╗..██╗....
 ██╔══██╗██╔══██╗████╗.████║██╔══██╗██║██║.....██╔════╝....██║..██╗..██║██║╚══██╔══╝██║..██║..
 ██║..╚═╝██║..██║██╔████╔██║██████╔╝██║██║.....█████╗......╚██╗████╗██╔╝██║...██║...███████║....
@@ -10,25 +17,25 @@ echo "
 ██╔══██╗██╔══██╗.██║..██╗..██║██╔════╝██╔══██╗..██║..
 ██████╔╝██║..██║.╚██╗████╗██╔╝█████╗..██████╔╝..██║.....
 ██╔═══╝.██║..██║..████╔═████║.██╔══╝..██╔══██╗..╚═╝...
-██║.....╚█████╔╝..╚██╔╝.╚██╔╝.███████╗██║..██║...██╗....
-╚═╝......╚════╝....╚═╝...╚═╝..╚══════╝╚═╝..╚═╝...╚═╝. "
-echo " Hello, Thank you for using this script "
-echo " You can easily build orange fox with this script for your Device "
-sleep 2
-echo " First lets setup the environment "
-echo " Press Enter to Start "
+██║.....╚█████╔╝..╚██╔╝.╚██╔╝.███████╗██║..██║..██╗....
+╚═╝......╚════╝....╚═╝...╚═╝..╚══════╝╚═╝..╚═╝..╚═╝. "
+echo " Hello, Thank you for using this script
+You can easily build orange fox with this script for your Device "
+sleep 5
+echo " First lets setup the environment
+Press Enter to Start "
 read -r Ans
-
-echo "_________________________________________________________________________________________"
+clear
+#---------------
 
 cd || return
 git clone https://github.com/akhilnarang/scripts
 bash scripts/setup/android_build_env.sh
+clear
+#---------------
 
-echo "_________________________________________________________________________________________"
-
-echo " Now lets sync the Latest Orange Fox Sources [ Latest 9.0 ] "
-echo " Press Enter to Start "
+echo " Now lets sync the Latest Orange Fox Sources [ Latest 9.0 ]
+Press Enter to Start "
 read -r Ans
 cd || return
 cd scripts || return
@@ -37,10 +44,9 @@ cd Orangefox || return
 repo init --depth=1 -q -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
 repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j"$(nproc --all)"
 
-echo "_________________________________________________________________________________________"
+#---------------
 
 clear
-
 echo "_________________________________________________________________________________________"
 
 echo " Now tell me your device codename "
@@ -48,84 +54,112 @@ read -r code
 
 echo " Now tell me your device vendor "
 read -r vendor
+
 echo " Now Give me your trees github link "
 read -r trees
 
-cd || return
-cd scripts/Orangefox || return
-git clone "$trees" device/"$vendor"/"$code"
-clear
-echo " If you have to edit the cloned tree (e.g. BoardConfig.mk) do it now. "
-echo " Press Enter to Start "
-read -r Ans
-
-echo "_________________________________________________________________________________________"
-
-echo " Now lets start building the environment "
-cd || return
-cd scripts/Orangefox/ || return
-source build/envsetup.sh
-
-echo "_________________________________________________________________________________________"
-
-echo " Now tell me the name of the maintainer "
+echo " Now tell me the your name (Username) "
 read -r main
 
-echo "_________________________________________________________________________________________"
+clear
+
+#-------------------------------------
+cd || return
+cd scripts/Orangefox || return
+git clone "${trees}" device/"${vendor}"/"${code}"
+clear
+
+echo " If you have to edit the cloned tree (e.g. BoardConfig.mk) do it now.
+Press Enter to Continue "
+read -r Ans
+
+clear
+
+#----------------------------
+
+echo " Now lets start building the environment "
+source build/envsetup.sh
+
+#-------------------------
 
 #Maintainer
-export OF_MAINTAINER="$main"
-
+export OF_MAINTAINER="${main}"
 ## Universal variables for building
 export ALLOW_MISSING_DEPENDENCIES=true
 export TW_DEFAULT_LANGUAGE="en"
-# To use ccache to speed up building
 export USE_CCACHE="1"
-# Enforced by R11 rules
 export FOX_R11="1"
 export FOX_ADVANCED_SECURITY="1"
 export FOX_RESET_SETTINGS="1"
-# Import OrangeFox build variables
-source ~/OrangeFox-Universal-Compile-Tool/configs/"${code}"_ofconfig
+
 echo " Now tell me the version "
 read -r version
 export FOX_VERSION="${version}"
 
-export | grep "export" >> "$code"_var-log.txt
+clear
+#-------------------------
+
+echo " Options
+ 1 : You already have a config file created by this script
+ 2 : You don't have a config file and now we will create it for you and will use it later when rebuilding Ofox
+ -------------------------------------------------------------------------------------------------------------- "
+
+read -r Ans3
+
+if [ "${Ans3}" = 1 ]
+then
+source ~/OrangeFox-Universal-Compile-Tool/configs/"${code}"_ofconfig
+echo " Done importing your Device Specific settings "
+clear
+#-----
+elif [ "${Ans3}" = 2 ]
+then
+echo " Lets create a config file using our good friend nano for your device var settings.
+(https://gitlab.com/OrangeFox/vendor/recovery/-/blob/master/orangefox_build_vars.txt) "
+
+touch ~/OrangeFox-Universal-Compile-Tool/configs/"${code}"_ofconfig | nano
+
+echo " Now that You've saved you're config fle, lets build Ofox
+Press enter when ready "
+read -r enter
+
+source ~/OrangeFox-Universal-Compile-Tool/configs/"${code}"_ofconfig
+fi
+
 
 clear
 echo "_________________________________________________________________________________________"
 
-echo " Lets Lunch it all together 😉😋 "
+echo " Lets Launch it together 😉😋 "
 lunch omni_"${code}"
 
 # If lunch command fail, there is no need to continue building
 if [ "$?" != "0" ]; then
+  echo "launch failed :/"
   exit
 fi
 
+clear
 
+echo " ^-^
+_________________________________________________________________________________________
 
-echo "_________________________________________________________________________________________"
-
-echo " Lets Start Building "
+Lets Start Building "
 mka recoveryimage
 
-echo "_________________________________________________________________________________________"
-
-echo " Thank you for using our Script "
-sleep 3
+#---------
 clear
-echo " credits :  "
-echo " Do follow my Github Account for more scripts : https://github.com/Sammy970 "
-echo " Do follow me too : https://github.com/gghhkm "
-echo " Would love to help you - Sammy970"
-echo " All the best - gghhkm "
-sleep 4
+
+echo " credits :
+Do follow my Github Account for more scripts : https://github.com/Sammy970
+Do follow me too : https://github.com/gghhkm
+Would love to help you - Sammy970
+All the best - gghhkm "
+sleep 5
 echo "
 ██████╗..█████╗.███╗..██╗███████╗..██╗.██╗...
 ██╔══██╗██╔══██╗████╗.██║██╔════╝..██║.██║....
 ██║..██║██║..██║██╔██╗██║█████╗....██║.██║..
 ██║..██║██║..██║██║╚████║██╔══╝....╚═╝.╚═╝.....
-██████╔╝╚█████╔╝██║.╚███║███████╗...██╗.██╗...
-╚═════╝..╚════╝.╚═╝..╚══╝╚══════╝...╚═╝.╚═╝....."
+██████╔╝╚█████╔╝██║.╚███║███████╗..██╗.██╗...
+╚═════╝..╚════╝.╚═╝..╚══╝╚══════╝..╚═╝.╚═╝....."
